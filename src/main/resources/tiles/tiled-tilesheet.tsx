d760bee7debc4531706aafca729e5d35566647c7
Преслав Стоянов <preslav0609@gmail.com>
2023-04-11 20:52:03 +0300

    Add some files

---
+++ src/main/resources/tiles/tiled-tilesheet.tsx
@@ -0,0 +1,106 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="всички" tilewidth="16" tileheight="16" tilecount="34" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="16" height="16" source="../dev/treasure-hunt/src/main/resources/tiles/01_water.png"/>
+ </tile>
+ <tile id="1">
+  <image width="16" height="16" source="../dev/treasure-hunt/src/main/resources/tiles/02_water.png"/>
+ </tile>
+ <tile id="2">
+  <image width="16" height="16" source="../dev/treasure-hunt/src/main/resources/tiles/03_water.png"/>
+ </tile>
+ <tile id="3">
+  <image width="16" height="16" source="../dev/treasure-hunt/src/main/resources/tiles/04_water.png"/>
+ </tile>
+ <tile id="4">
+  <image width="16" height="16" source="../dev/treasure-hunt/src/main/resources/tiles/05_water.png"/>
+ </tile>
+ <tile id="5">
+  <image width="16" height="16" source="../dev/treasure-hunt/src/main/resources/tiles/06_water.png"/>
+ </tile>
+ <tile id="6">
+  <image width="16" height="16" source="../dev/treasure-hunt/src/main/resources/tiles/07_water.png"/>
+ </tile>
+ <tile id="7">
+  <image width="16" height="16" source="../dev/treasure-hunt/src/main/resources/tiles/08_water.png"/>
+ </tile>
+ <tile id="8">
+  <image width="16" height="16" source="../dev/treasure-hunt/src/main/resources/tiles/09_water.png"/>
+ </tile>
+ <tile id="9">
+  <image width="16" height="16" source="../dev/treasure-hunt/src/main/resources/tiles/10_water.png"/>
+ </tile>
+ <tile id="10">
+  <image width="16" height="16" source="../dev/treasure-hunt/src/main/resources/tiles/11_water.png"/>
+ </tile>
+ <tile id="11">
+  <image width="16" height="16" source="../dev/treasure-hunt/src/main/resources/tiles/12_water.png"/>
+ </tile>
+ <tile id="12">
+  <image width="16" height="16" source="../dev/treasure-hunt/src/main/resources/tiles/13_water.png"/>
+ </tile>
+ <tile id="13">
+  <image width="16" height="16" source="../dev/treasure-hunt/src/main/resources/tiles/14_road.png"/>
+ </tile>
+ <tile id="14">
+  <image width="16" height="16" source="../dev/treasure-hunt/src/main/resources/tiles/15_road.png"/>
+ </tile>
+ <tile id="15">
+  <image width="16" height="16" source="../dev/treasure-hunt/src/main/resources/tiles/16_road.png"/>
+ </tile>
+ <tile id="16">
+  <image width="16" height="16" source="../dev/treasure-hunt/src/main/resources/tiles/17_road.png"/>
+ </tile>
+ <tile id="17">
+  <image width="16" height="16" source="../dev/treasure-hunt/src/main/resources/tiles/18_road.png"/>
+ </tile>
+ <tile id="18">
+  <image width="16" height="16" source="../dev/treasure-hunt/src/main/resources/tiles/19_road.png"/>
+ </tile>
+ <tile id="19">
+  <image width="16" height="16" source="../dev/treasure-hunt/src/main/resources/tiles/20_road.png"/>
+ </tile>
+ <tile id="20">
+  <image width="16" height="16" source="../dev/treasure-hunt/src/main/resources/tiles/21_road.png"/>
+ </tile>
+ <tile id="21">
+  <image width="16" height="16" source="../dev/treasure-hunt/src/main/resources/tiles/22_road.png"/>
+ </tile>
+ <tile id="22">
+  <image width="16" height="16" source="../dev/treasure-hunt/src/main/resources/tiles/23_road.png"/>
+ </tile>
+ <tile id="23">
+  <image width="16" height="16" source="../dev/treasure-hunt/src/main/resources/tiles/24_road.png"/>
+ </tile>
+ <tile id="24">
+  <image width="16" height="16" source="../dev/treasure-hunt/src/main/resources/tiles/25_road.png"/>
+ </tile>
+ <tile id="25">
+  <image width="16" height="16" source="../dev/treasure-hunt/src/main/resources/tiles/26_road.png"/>
+ </tile>
+ <tile id="26">
+  <image width="16" height="16" source="../dev/treasure-hunt/src/main/resources/tiles/27_bridge.png"/>
+ </tile>
+ <tile id="27">
+  <image width="16" height="16" source="../dev/treasure-hunt/src/main/resources/tiles/28_bridge2.png"/>
+ </tile>
+ <tile id="28">
+  <image width="16" height="16" source="../dev/treasure-hunt/src/main/resources/tiles/29_earth.png"/>
+ </tile>
+ <tile id="29">
+  <image width="16" height="16" source="../dev/treasure-hunt/src/main/resources/tiles/30_floor.png"/>
+ </tile>
+ <tile id="30">
+  <image width="16" height="16" source="../dev/treasure-hunt/src/main/resources/tiles/31_grass.png"/>
+ </tile>
+ <tile id="31">
+  <image width="16" height="16" source="../dev/treasure-hunt/src/main/resources/tiles/32_tree.png"/>
+ </tile>
+ <tile id="32">
+  <image width="16" height="16" source="../dev/treasure-hunt/src/main/resources/tiles/33_tree2.png"/>
+ </tile>
+ <tile id="33">
+  <image width="16" height="16" source="../dev/treasure-hunt/src/main/resources/tiles/34_wall.png"/>
+ </tile>
+</tileset>
